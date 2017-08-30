0eeeeeab62eaed4928cbe10add3082e0dc34229b
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-30 21:11:13 +0800

    Create class_2.1.py

---
+++ class_2.1.py
@@ -0,0 +1,11 @@
+    def praise(self):
+        return "You inspire me, {}".format(self.name)
+    
+    def reassurance(self):
+        return "Chin up, {}. You'll get it next time!".format(self.name)
+    
+    def feedback(self, grade):
+        if grade > 50:
+            return self.praise()
+        else:
+            return self.reassurance()
