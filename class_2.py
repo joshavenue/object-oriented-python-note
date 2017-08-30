ad6135452988253162fe87faa619f68091d42b08
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-30 21:08:20 +0800

    Update class_2.py

--- class_2.py
+++ class_2.py
@@ -1,8 +1,12 @@
 import random
 class Evil:
     stealing = True
-
+    evil_name = 'Harry'
+    
     def stealing_time(self):                  // self is often used //
         if self.stealing:
             return bool(random.randint(0,1))
         return False
+
+    def achievement(self):
+        return 'I got the stuff, {}!'.format(self.evil_name) // Self.variable to point it back up
