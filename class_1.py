fb8e41b0dfd3d414e2c793a4df7eb6d100f06055
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-30 20:46:41 +0800

    Create class_1.py

---
+++ class_1.py
@@ -0,0 +1,20 @@
+class Apple:
+  edible = True
+  
+// Open Bash terminal and find directory to where file is kept //
+// cd 'File location' //
+// from 'Class PY file origin' import 'Class Name' //
+// In this case --> from class_1 import Apple //
+
+// >>> from class_1 import Apple
+// >>> whatever = Apple()
+// >>> whatever.edible
+// TRUE
+// >> Apple.edible
+// TRUE
+// >> whatever.edible = False         // You can change instances outcome without affecting the class
+// >> whatever.edible
+// FALSE                              // As shown below
+// >> Apple.edible
+// TRUE
+
