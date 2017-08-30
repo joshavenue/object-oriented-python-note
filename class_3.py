02de5a07a9e470e8452a464cf76f0410df2f65ee
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-30 21:29:08 +0800

    Create class_3.py

---
+++ class_3.py
@@ -0,0 +1,32 @@
+import random
+class Evil:
+    stealing = True
+
+    def __init__(self,name,stealing = True, **kwargs):            // **kwargs is keyword argument
+        self.name = name                                          // This allows you to add a name
+        self.stealing = stealing
+
+        for key, value in kwargs.items():                         // This allow you to add new information in your Class
+            setattr(self,key,value)                               // Set Attribute with anything, add anything you like
+
+    def stealing_time(self):
+        return self.stealing and bool(random.randint(0,1))
+
+    def hide(self, light_level):
+        return self.stealing and light_level < 10
+        
+        
+  
+// Run Bash terminal 
+// Run Python
+
+>> from class_3 import Evil
+>> whatever = Evil()
+>> whatever = Evil('Bob', gun='Pistol', ammo=30, strength=10')
+>> whatever.name
+Bob
+>> whatever.gun
+Pistol
+>> whatever.ammo
+30
+
